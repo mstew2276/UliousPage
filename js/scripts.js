ea9029bddff267bbb101c304b2cdffdd03b5f905
Mason Stewart <masonstewart2276@gmail.com>
2023-06-18 19:55:50 -0700

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    });
+
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
+    });
+});
